9269db3ff4e769ea6dbdc1a7afba02c9839a607c
Germain Leignel <germain.leignel@gmail.com>
2020-03-28 16:47:07 +0000

    not sure

--- mobile/dequarantine/lib/UI/pages/home_page.dart
+++ mobile/dequarantine/lib/UI/pages/home_page.dart
@@ -1,7 +1,5 @@
 
-import 'package:dequarantine/constants.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 
 class Home extends StatefulWidget {
   @override
