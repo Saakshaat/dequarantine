3063faf739dee99bf9192659ff5ab154f9971c95
Hamza Tahir <mac@Hamza-Tahir.local>
2020-04-02 21:38:29 +0500

    Front end files by hamza

--- backend/functions/util/admin.js
+++
@@ -1,7 +0,0 @@
-const admin = require('firebase-admin');
-
-admin.initializeApp();
-
-const db = admin.firestore();
-
-module.exports = { admin, db };
--- mobile/dequarantine/lib/logic/functions/sign_out_functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dequarantine/main.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
-void signOut(BuildContext context) {
-  Fluttertoast.showToast(msg: "Signing you out");
-  currentUser.clean();
-  Navigator.pushNamedAndRemoveUntil(context, "/login", (Route<dynamic> route) => false);
-}
