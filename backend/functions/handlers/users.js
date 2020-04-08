9db9c2ebffc8a1f9d4203d3874160d0b15eb96c8
Saakshaat <saakshaat2001@gmail.com>
2020-04-08 12:09:04 -0400

    Fixing Get Attending Events to include event ID

--- backend/functions/handlers/users.js
+++ backend/functions/handlers/users.js
@@ -1,36 +1,38 @@
 const { admin, db } = require("../util/admin");
 const config = require("../util/config");
 
-const firebase = require('firebase');
+const firebase = require("firebase");
 firebase.initializeApp(config);
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
 const {
-    validateSignUpData,
-    validateLoginData,
-    reduceUserDetails
+  validateSignUpData,
+  validateLoginData,
+  reduceUserDetails
 } = require("../util/validators");
 
 //User Signup [New Account Creation]
 exports.signup = (req, res) => {
-    const newUser = {
-        email: req.body.email,
-        password: req.body.password,
-        confirmPassword: req.body.confirmPassword,
-        userName: req.body.userName
-      };
-    
-    const { valid, errors } = validateSignUpData(newUser);
-    if (!valid) return res.status(400).json(errors);
-    const noImg = `no-img.png`;
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userName: req.body.userName
+  };
 
-    let token, userId;
-    db.doc(`/users/${newUser.userName}`)
+  const { valid, errors } = validateSignUpData(newUser);
+  if (!valid) return res.status(400).json(errors);
+  const noImg = `no-img.png`;
+
+  let token, userId;
+  db.doc(`/users/${newUser.userName}`)
     .get()
     .then(doc => {
       if (doc.exists) {
-        return res.status(400).json({ userName: `this User Name is already taken` });
+        return res
+          .status(400)
+          .json({ userName: `this User Name is already taken` });
       } else {
         return firebase
           .auth()
@@ -71,106 +73,116 @@ exports.signup = (req, res) => {
 
 //Manual Login using email and password
 exports.login = (req, res) => {
-    const user = {
-        email: req.body.email,
-        password: req.body.password
-    };
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  };
 
-    const { valid, errors } = validateLoginData(user);
-    if (!valid) return res.status(400).json(errors);
+  const { valid, errors } = validateLoginData(user);
+  if (!valid) return res.status(400).json(errors);
 
-    firebase
-        .auth()
-        .signInWithEmailAndPassword(user.email, user.password)
-        .then(data => {
-        return data.user.getIdToken();
-        })
-        .then(token => {
-        return res.json({ token });
-        })
-        .catch(err => {
-        console.error(err);
-        return res
-            .status(403)
-            .json({ general: `Wrong credentials, please try again` });
-        });
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      return res
+        .status(403)
+        .json({ general: `Wrong credentials, please try again` });
+    });
 };
-
 
 //Password Forget Request | User gets an email from a support account with Password Reset Link
 exports.forgotPassword = (req, res) => {
-    firebase
-      .auth()
-      .sendPasswordResetEmail(req.body.email)
-      .then(data => {
-        console.log("Password reset link sent");
-      })
-      .catch(err => {
-        return res
-          .status(400)
-          .json({
-            general: `Couldn't send reset link. Try to remember password`
-          });
+  firebase
+    .auth()
+    .sendPasswordResetEmail(req.body.email)
+    .then(data => {
+      console.log("Password reset link sent");
+    })
+    .catch(err => {
+      return res.status(400).json({
+        general: `Couldn't send reset link. Try to remember password`
       });
-  };
+    });
+};
 
 //Google Signin with a Pop-Up
 /**
  * TODO: Make working Google Signin which
  * - Creates a new object in the user collection if
  *  * no user object exists with the returned email
- *  
+ *
  * - If there is a user account with the same email as returned by the Google API, then
  *  * Sign them in
  *  * Get the generated token
  *  * Use the token to authorize access person and keep them signed in
  */
 exports.googleSignin = (req, res) => {
-    const credential = firebase.auth.GoogleAuthProvider.credential(null, req.body.access_token);
+  const credential = firebase.auth.GoogleAuthProvider.credential(
+    null,
+    req.body.access_token
+  );
 
-    firebase.auth().signInWithCredential(credential)
-    .then((result)=> {
-        let newUser = {};
-        newUser.email = result.user.email;
-        newUser.userName = result.user.displayName.replace(/\s/g, '');
+  firebase
+    .auth()
+    .signInWithCredential(credential)
+    .then(result => {
+      let newUser = {};
+      newUser.email = result.user.email;
+      newUser.userName = result.user.displayName.replace(/\s/g, "");
 
-        db.collection(`users`)
-        .where('email', '==', newUser.email)
+      db.collection(`users`)
+        .where("email", "==", newUser.email)
         .get()
         .then(querySnapshot => {
-            if(querySnapshot.size > 0) {
-                console.log('User Exists. Do nothing.\n'); //, querySnapshot.docs[0].data());
-            } else { //Create a new user in docs/user
-                const noImg = `no-img.png`;
-                attending = [];
-                const userCredentials = {
-                    userName: newUser.userName,
-                    email: newUser.email,
-                    imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
-                    createdAt: new Date().toISOString(),
-                    userId: result.user.uid,
-                    attending
-                };
+          if (querySnapshot.size > 0) {
+            console.log("User Exists. Do nothing.\n"); //, querySnapshot.docs[0].data());
+          } else {
+            //Create a new user in docs/user
+            const noImg = `no-img.png`;
+            attending = [];
+            const userCredentials = {
+              userName: newUser.userName,
+              email: newUser.email,
+              imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+              createdAt: new Date().toISOString(),
+              userId: result.user.uid,
+              attending
+            };
 
-                db.doc(`/users/${newUser.userName}`).set(userCredentials);
-            }
+            db.doc(`/users/${newUser.userName}`).set(userCredentials);
+          }
         })
         .then(() => {
-            return res.status(201).json({ refreshToken: result.user.refreshToken, token: req.body.access_token });
+          return res
+            .status(201)
+            .json({
+              refreshToken: result.user.refreshToken,
+              token: req.body.access_token
+            });
         });
     })
     .catch(function(error) {
-        // console.log('ERROR\n-----------------\n', error);
-        let errorCode = error.code;
-        let errorMessage = error.message;
-        let email = error.email;
-        let authCredential = error.credential;
+      // console.log('ERROR\n-----------------\n', error);
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let authCredential = error.credential;
 
-        if (errorCode == 'auth/invalid-credential') {
-            return res.status(400).send("Invalid Credentials. Please log in again.");
-        } else {
-            return res.status(400).send("Error. Please try again.");
-        }
+      if (errorCode == "auth/invalid-credential") {
+        return res
+          .status(400)
+          .send("Invalid Credentials. Please log in again.");
+      } else {
+        return res.status(400).send("Error. Please try again.");
+      }
     });
 };
 
@@ -271,11 +283,12 @@ exports.uploadImage = (req, res) => {
       });
   });
   busboy.end(req.rawBody);
-}
+};
 
 //Get all the events that the user is attending
 exports.getAttending = (req, res) => {
   const userName = req.user.userName;
+  let events = [];
   db.doc(`/users/${userName}`)
     .get()
     .then(doc => {
@@ -286,10 +299,21 @@ exports.getAttending = (req, res) => {
           .get();
       }
     })
-    .then(data => {
-      let events = [];
-      data.forEach(doc => {
-        events.push(doc.data());
+    .then(doc => {
+      doc.forEach(data => {
+        events.push({
+          eventId: data.id,
+          imageUrl: data.data().imageUrl,
+          name: data.data().name,
+          organizer: data.data().organizer,
+          time: data.data().time,
+          cap: data.data().cap,
+          link: data.data().link,
+          attending: data.data().attending,
+          category: data.data().category,
+          description: data.data().description,
+          participants: data.data().participants
+        });
       });
       return res.json(events);
     })
@@ -297,4 +321,4 @@ exports.getAttending = (req, res) => {
       console.error(err);
       return res.status(500).json({ error: err.code });
     });
-}
+};
