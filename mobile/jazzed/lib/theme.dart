7cf064aa2a3ebfad739f8a12b4aacccfd2b342c5
Germain Leignel <germain.leignel@gmail.com>
2020-04-22 17:51:19 +0100

    just some colors

--- mobile/jazzed/lib/theme.dart
+++ mobile/jazzed/lib/theme.dart
@@ -26,4 +26,10 @@ class CustomColorTheme {
   Color electricBlue = Color.fromRGBO(0, 232, 252, 1);
   Color yellowRed = Color.fromRGBO(246, 189, 87, 1);
   Color blackCharcoal = Color.fromRGBO(84, 88, 99, 1);
+
+  Color blue = Color.fromRGBO(36, 123, 160, 1);
+  Color blueGreen = Color.fromRGBO(112, 193, 179, 1);
+  Color dark = Color.fromRGBO(36, 58, 63, 1);
+  Color pale = Color.fromRGBO(243, 255, 189, 1);
+  Color red = Color.fromRGBO(255, 22, 84, 1);
 }
